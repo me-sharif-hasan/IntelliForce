7dcec614614724c299352e870a1dc458637ecfc9
Sharif Hasan <me.sharif.hasan@gmail.com>
2025-05-02 23:44:21 +0600

    apex grammer added

--- src/main/java/com/iishanto/language/sf/parser/ApexClassParserDefinition.java
+++ src/main/java/com/iishanto/language/sf/parser/ApexClassParserDefinition.java
@@ -1,18 +1,10 @@
 package com.iishanto.language.sf.parser;
 
-//import com.iishanto.antlr.ApexLexer;
-//import com.iishanto.antlr.ApexParser;
-import com.iishanto.antlr.ApexParser;
 import com.iishanto.language.sf.ApexLanguage;
-//import com.iishanto.language.sf.apex.psi.ApexTypes;
-//import com.iishanto.language.sf.apex.psi.ApexTypes;
+import com.iishanto.language.sf.apex.psi.ApexClassFile;
+import com.iishanto.language.sf.apex.psi.ApexTypes;
+import com.iishanto.language.sf.lexer.ApexLexer;
 import com.iishanto.language.sf.lexer.BnfApexLexer;
-import com.iishanto.language.sf.parser.manual.ApexManualParser;
-import com.illuminatedcloud.intellij.apex.fileType.ApexClassFile;
-import com.illuminatedcloud.intellij.apex.lexer.ApexLexer;
-import com.illuminatedcloud.intellij.apex.psi.ApexType;
-import com.illuminatedcloud.intellij.apex.psi.ApexTypes;
-import com.illuminatedcloud.intellij.apex.psi.ApexTypes$Factory;
 import com.intellij.lang.*;
 import com.intellij.lang.java.lexer.JavaLexer;
 import com.intellij.lexer.Lexer;
@@ -35,40 +27,20 @@ import org.jetbrains.annotations.NotNull;
 
 public class ApexClassParserDefinition implements ParserDefinition {
     private static final TokenSet WHITE_SPACES = TokenSet.create(TokenType.WHITE_SPACE);
-    private static final TokenSet COMMENTS = TokenSet.create(ApexTypes.LINE_COMMENT, ApexTypes.BLOCK_COMMENT);
+    private static final TokenSet COMMENTS = TokenSet.create(ApexTypes.LINE_COMMENT, ApexTypes.COMMENT);
 
     public static final IFileElementType FILE = new IFileElementType(Language.findInstance(ApexLanguage.class));
 
-    public static void main(String[] args) {
-//        new com.illuminatedcloud.intellij.apex.lexer.ApexLexer();
-//        new com.illuminatedcloud.intellij.apex.parser.ApexParser();
-
-    }
 
     @NotNull
     @Override
     public Lexer createLexer(Project project) {
         return new ApexLexer();
-//        String sourceCode = "public class Test {}";
-//        CharStream charStream = CharStreams.fromString(sourceCode);
-//        ApexLexer apexLexer=new ApexLexer(null);
-//        return new ANTLRLexerAdaptor(ApexLanguage.INSTANCE, apexLexer);
     }
 
     @Override
     public @NotNull PsiParser createParser(Project project) {
         return new com.iishanto.language.sf.apex.parser.ApexParser();
-//        ApexParser apexParser = new ApexParser(null);
-//        return new ANTLRParserAdaptor(ApexLanguage.INSTANCE, apexParser) {
-//            @Override
-//            protected ParseTree parse(Parser parser, IElementType iElementType) {
-//                System.out.println("Parse called " + parser.getClass().getName() + " " + iElementType.toString());
-//                if ( iElementType instanceof IFileElementType ) {
-//                    return ((ApexParser) parser).primary();
-//                }
-//                return ((ApexParser)parser).primary();
-//            }
-//        };
     }
 
     @Override
@@ -97,13 +69,12 @@ public class ApexClassParserDefinition implements ParserDefinition {
     @NotNull
     @Override
     public PsiElement createElement(ASTNode node) {
-        return ApexTypes$Factory.createElement(node);
-//        return new ANTLRPsiNode(node);
+        return ApexTypes.Factory.createElement(node);
     }
 
     @Override
     public @NotNull PsiFile createFile(@NotNull FileViewProvider viewProvider) {
-        return new ApexClassFile(viewProvider,null);
+        return new ApexClassFile(viewProvider);
     }
 
     @Override
